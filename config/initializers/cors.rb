d60dff0435427eceb66efb115ac99e64a3969407
Gabriel Ribeiro Pires <gabrielribeiropires@Gabriels-MacBook-Air.local>
2025-12-03 23:37:43 -0300

    Allow CORS from production domains

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,17 @@
 # config/initializers/cors.rb
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      # Permite que seu frontend Next.js na porta 3001 faça requisições
-      origins 'http://localhost:3001'
-  
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-    end
+  allow do
+    origins(
+      'http://localhost:3001',                    # dev
+      'http://localhost:3000',                    # (se usar essa porta em dev, opcional)
+      'https://thegrinderspowerlifting.com.br',   # domínio sem www
+      'https://www.thegrinderspowerlifting.com.br' # domínio com www
+    )
+
+    resource '*',
+      headers: :any,
+      methods: %i[get post put patch delete options head],
+      credentials: true
   end
+end
