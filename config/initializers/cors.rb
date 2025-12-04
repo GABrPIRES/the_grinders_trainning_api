5af2b1d863fbcccae22e97f0e98acff3b34a2ee9
Gabriel Ribeiro Pires <gabrielribeiropires@Gabriels-MacBook-Air.local>
2025-12-03 23:44:55 -0300

    Configura CORS para dominios de producao

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,17 +1,18 @@
 # config/initializers/cors.rb
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins(
-      'http://localhost:3001',                    # dev
-      'http://localhost:3000',                    # (se usar essa porta em dev, opcional)
-      'https://thegrinderspowerlifting.com.br',   # domínio sem www
-      'https://www.thegrinderspowerlifting.com.br' # domínio com www
-    )
-
-    resource '*',
-      headers: :any,
-      methods: %i[get post put patch delete options head],
-      credentials: true
+    allow do
+      # Permite que seu frontend Next.js na porta 3001 faça requisições
+      origins(
+        'http://localhost:3001',                    # dev
+        'http://localhost:3000',                    # (se usar essa porta em dev, opcional)
+        'https://thegrinderspowerlifting.com.br',   # domínio sem www
+        'https://www.thegrinderspowerlifting.com.br' # domínio com www
+      )
+  
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
   end
-end
